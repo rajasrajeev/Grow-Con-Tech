116e9e83f6ec3d9409a863f6d2394cf1bb7e7906
Digitintech admin <admin@digitintech.com>
2024-06-22 13:53:30 +0530

    seeder updates

--- prisma/seeds/seeds.js
+++ prisma/seeds/seeds.js
@@ -2,30 +2,76 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-
-  // Create a state
-  const country = await prisma.country.create({
+  // Create states
+  const state1 = await prisma.state.create({
     data: {
-      name: 'India'
-    },
-  });
-  // Create a state
-  const state = await prisma.state.create({
-    data: {
-      name: 'Kerala',
-      countryId: country.id
-    },
+      name: 'State 1',
+      districts: {
+        create: [
+          {
+            name: 'District 1-1',
+            /* vendors: {
+              create: [
+                { name: 'Vendor 1-1-1' },
+                { name: 'Vendor 1-1-2' }
+              ]
+            } */
+          },
+          {
+            name: 'District 1-2',
+            /* vendors: {
+              create: [
+                { name: 'Vendor 1-2-1' },
+                { name: 'Vendor 1-2-2' }
+              ]
+            } */
+          }
+        ]
+      },
+      /* vendors: {
+        create: [
+          { name: 'Vendor 1-1' },
+          { name: 'Vendor 1-2' }
+        ]
+      } */
+    }
   });
 
-  // Create a city
-  const city = await prisma.city.create({
+  const state2 = await prisma.state.create({
     data: {
-      name: 'Attingal',
-      stateId: state.id
-    },
+      name: 'State 2',
+      districts: {
+        create: [
+          {
+            name: 'District 2-1',
+            /* vendors: {
+              create: [
+                { name: 'Vendor 2-1-1' },
+                { name: 'Vendor 2-1-2' }
+              ]
+            } */
+          },
+          {
+            name: 'District 2-2',
+            /* vendors: {
+              create: [
+                { name: 'Vendor 2-2-1' },
+                { name: 'Vendor 2-2-2' }
+              ]
+            } */
+          }
+        ]
+      },
+      /* vendors: {
+        create: [
+          { name: 'Vendor 2-1' },
+          { name: 'Vendor 2-2' }
+        ]
+      } */
+    }
   });
 
-  console.log({ state, city, country });
+  console.log({ state1, state2 });
 }
 
 main()
