a61f8e4d2794687e1ba4cae35b523b2dc385419e
Digitintech admin <admin@digitintech.com>
2024-06-24 20:38:02 +0530

    product list update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,12 +9,12 @@ const {
     dailyRatesListForVendor,
     updateDailyRatesForVendor
 } = require("../services/product.service");
-// const productSchema = require('../schemas/product.schema');
+const productSchema = require('../schemas/product.schema');
 
 
 const getProductsHandler = async(req, res, next) => {
     try {
-        const data = await getProductsForUser(req.user.role, parseInt(req.user.id, 10), req.query);
+        const data = await getProductsForUser(req.query.role, parseInt(req.user.id, 10) , req.query);
         return res.status(200).send(data);
     } catch(err) {
         next(err);
