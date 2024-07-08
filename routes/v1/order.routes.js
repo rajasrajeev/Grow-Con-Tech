539cf8c72cf1fdd652587fd59f21bbb0724bd053
SRenjith1996 <renjithmanamboor123@gmail.com>
2024-07-08 12:34:49 +0530

    bug

--- routes/v1/order.routes.js
+++ routes/v1/order.routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 const uploads = uploadFiles.fields([{name: 'e_bill', maxCount: 1},{name: 'e_way_bill', maxCount: 30}]);
 
 module.exports = (app) => {
-    router.patch('/:id', userAuth, checkRole['VENDOR'], uploads, uploadEBillsHandler)
+    router.patch('/:id', userAuth, checkRole(['VENDOR']), uploads, uploadEBillsHandler);
     router.get('/:id', userAuth, getOrderDetailsHandler);
     router.get('/', userAuth, getOrdersHandler);
     
